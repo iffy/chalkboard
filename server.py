2d9c3f306f5178b34315dd3d536f0c121739d584
Matt Haggard <haggardii@gmail.com>
2013-01-30 15:57:21 -0700

    Database-backed storage works

--- server.py
+++ server.py
@@ -1,5 +1,8 @@
 from twisted.web.resource import Resource
 from twisted.web.server import NOT_DONE_YET
+from twisted.internet import defer
+from twisted.enterprise.adbapi import ConnectionPool
+from twisted.python import log
 from collections import defaultdict
 from functools import partial
 from jinja2 import Environment, FileSystemLoader
@@ -11,6 +14,174 @@ def render(name, params=None):
     template = jenv.get_template(name)
     return template.render(params or {}).encode('utf-8')
 
+
+
+def getReadySqlite(connstr):
+    pool = ConnectionPool('pysqlite2.dbapi2', connstr,
+                          cp_min=1, cp_max=1)
+    def interaction(c):
+        try:
+            c.execute('''create table sticky (
+                    id integer primary key,
+                    board_id integer,
+                    updated timestamp default current_timestamp,
+                    note text,
+                    x integer,
+                    y integer)''')
+        except Exception as e:
+            log.err(e)
+    return pool.runInteraction(interaction).addCallbacks((lambda x:pool), log.err)
+
+
+
+class DatabaseBoardStore(object):
+
+    paramstyle = '%s'
+    tablename = 'sticky'
+    pool = None
+
+
+    def __init__(self, paramstyle='%s'):
+        self.pending = []
+        self.paramstyle = paramstyle
+
+
+    def attachPool(self, pool):
+        self.pool = pool
+        self.runPending()
+
+
+    def runPending(self):
+        if not self.pool:
+            return
+        for d,name,args in self.pending:
+            print 'd', name, args
+            method = getattr(self.pool, name)
+            method(*args).addBoth(lambda x:d.callback(x))
+        while self.pending:
+            self.pending.pop()
+
+    def queue(self, name, args):
+        d = defer.Deferred()
+        self.pending.append((d, name, args))
+        self.runPending()
+        return d
+
+
+    def runQuery(self, *args):
+        return self.queue('runQuery', args)
+
+
+    def runInteraction(self, *args):
+        return self.queue('runInteraction', args)
+
+
+    def formatRawQuery(self, qry):
+        return qry % {'param': self.paramstyle}
+
+
+    def getStickies(self, board_id):
+        qry = self.formatRawQuery('select * from sticky '
+                                  'where board_id = %(param)s '
+                                  'order by updated')
+        return self.runQuery(qry, (board_id,)).addCallback(self._gotStickies)
+
+
+    def getSticky(self, sticky_id, board_id):
+        select = self.formatRawQuery('''select * from sticky
+                                      where id=%(param)s and board_id=%(param)s''')
+        return self.runQuery(select, (sticky_id, board_id)).addCallback(self._gotOneRow)
+
+
+    def _gotStickies(self, results):
+        return [self._gotOneRow([x]) for x in results]
+
+
+    def addSticky(self, board_id, note, x, y):
+        insert = self.formatRawQuery('''insert into sticky (board_id, note, x, y)
+                                     values(%(param)s, %(param)s, %(param)s, %(param)s)''')
+        def interaction(c):
+            c.execute(insert, (board_id, note, x, y))
+            return c.lastrowid
+        
+        d = self.runInteraction(interaction)
+        return d.addCallback(self.getSticky, board_id)
+
+
+    def _gotOneRow(self, row):
+        log.msg('Got row: %r' % (row,))
+        row = row[0]
+        id, board_id, updated, note, x, y = row
+        return {
+            'id': id,
+            'note': note,
+            'x': x,
+            'y': y,
+        }
+
+
+    def updateSticky(self, board_id, sticky_id, note, x, y):
+        update = self.formatRawQuery('''update sticky set note=%(param)s,
+                                        x=%(param)s, y=%(param)s,
+                                        updated=current_timestamp
+                                        where board_id=%(param)s and id=%(param)s''')
+        d = self.runQuery(update, (note, x, y, board_id, sticky_id))
+        d.addCallback(lambda x:sticky_id)
+        return d.addCallback(self.getSticky, board_id)
+
+
+
+class EventStoreWrapper(object):
+    """
+    I provide event notification to a store.
+    """
+    
+    def __init__(self, store):
+        self.store = store
+        self.subscriptions = defaultdict(lambda:[])
+
+
+    def getStickies(self, board_id):
+        return defer.maybeDeferred(self.store.getStickies, board_id)
+
+
+    def addSticky(self, board_id, *args):
+        return defer.maybeDeferred(self.store.addSticky,
+                                   board_id, *args).addCallback(self.emitOne, board_id, 'add')
+
+
+    def updateSticky(self, board_id, *args):
+        return defer.maybeDeferred(self.store.updateSticky,
+                                   board_id, *args).addCallback(self.emitOne, board_id, 'update')
+
+
+    def removeSticky(self, board_id, *args):
+        return defer.maybeDeferred(self.store.removeSticky,
+                                   board_id, *args).addCallback(self.emitOne, board_id, 'remove')
+
+
+    def emit(self, board_id, action, data):
+        for s in list(self.subscriptions[board_id]):
+            try:
+                log.msg('emit %r %r %r' % (board_id, action, data))
+                s(action, data)
+            except Exception as e:
+                print 'Error sending to subscription %s' % (e,)
+                self.unsubscribe(board_id, s)
+
+
+    def subscribe(self, board_id, func):
+        self.subscriptions[board_id].append(func)
+
+
+    def unsubscribe(self, board_id, func):
+        self.subscriptions[board_id].remove(func)
+
+
+    def emitOne(self, record, board_id, action):
+        self.emit(board_id, action, record)
+        return record
+        
 
 
 class InMemoryBoardStore(object):
@@ -31,58 +202,48 @@ class InMemoryBoardStore(object):
         return self.boards[board_id]
 
 
-    def addSticky(self, board_id, text, x, y):
+    def addSticky(self, board_id, note, x, y):
         id = self.nextId()
         self.boards[board_id].append({
             'id': id,
-            'text': text,
+            'note': note,
             'x': x,
             'y': y,
         })
-        ret = self.boards[board_id][-1]
-        self.emit(board_id, 'add', ret)
-        return ret
+        sticky = self.boards[board_id][-1]
+        return sticky
 
 
     def removeSticky(self, board_id, sticky_id):
-        found = [x for x in self.boards[board_id] if x['id'] == sticky_id]
-        for f in found:
-            self.boards[board_id].remove(f)
-            self.emit(board_id, 'remove', f)
+        found = [x for x in self.boards[board_id] if x['id'] == sticky_id][0]
+        self.boards[board_id].remove(found)
+        return sticky_id
 
 
-    def updateSticky(self, board_id, sticky_id, text, x, y):
-        found = [s for s in self.boards[board_id] if s['id'] == sticky_id]
-        for f in found:
-            f.update({
-                'text': text,
-                'x': x,
-                'y': y,
-            })
-            self.boards[board_id].remove(f)
-            self.boards[board_id].append(f)
-            self.emit(board_id, 'update', f)
-
-    
-    def emit(self, board_id, action, data):
-        for s in list(self.subscriptions[board_id]):
-            try:
-                s(action, data)
-            except Exception as e:
-                print 'Error sending to subscription %s' % (e,)
-                self.unsubscribe(board_id, s)
-
-
-    def subscribe(self, board_id, func):
-        self.subscriptions[board_id].append(func)
-
-
-    def unsubscribe(self, board_id, func):
-        self.subscriptions[board_id].remove(func)
+    def updateSticky(self, board_id, sticky_id, note, x, y):
+        sticky = [s for s in self.boards[board_id] if s['id'] == sticky_id][0]
+        sticky.update({
+            'note': note,
+            'x': x,
+            'y': y,
+        })
+        self.boards[board_id].remove(sticky)
+        self.boards[board_id].append(sticky)
+        return sticky
 
 
 
-store = InMemoryBoardStore()
+#store = EventStoreWrapper(InMemoryBoardStore())
+_store = DatabaseBoardStore('?')
+getReadySqlite('/tmp/d.sqlite').addCallback(_store.attachPool)
+store = EventStoreWrapper(_store)
+
+
+
+def jsonfinish(data, request):
+    request.setHeader('Content-type', 'application/json')
+    request.write(json.dumps(data).encode('utf-8'))
+    request.finish()
 
 
 class Chalkboard(Resource):
@@ -110,19 +271,19 @@ class Chalkboard(Resource):
 
 
     def do_add(self, request):
-        text = request.args.get('text')[0]
+        note = request.args.get('note')[0]
         x = int(request.args.get('x')[0])
         y = int(request.args.get('y')[0])
-        s = store.addSticky(self.board_id, text, x, y)
-        return json.dumps(s).encode('utf-8')
+        store.addSticky(self.board_id, note, x, y).addCallback(jsonfinish, request)
+        return NOT_DONE_YET
 
 
     def do_update(self, request):
         sticky_id = int(request.args.get('id')[0])
-        text = request.args.get('text')[0]
+        note = request.args.get('note')[0]
         x = int(request.args.get('x')[0])
         y = int(request.args.get('y')[0])
-        store.updateSticky(self.board_id, sticky_id, text, x, y)
+        store.updateSticky(self.board_id, sticky_id, note, x, y)
         return ''
 
 
@@ -144,14 +305,18 @@ class StickyFeed(Resource):
     def render_GET(self, request):
         request.setHeader('Content-type', 'text/event-stream')
         request.write(sseMsg('hello', 'keep alive'))
-        stickies = store.getStickies(self.board_id)
-        for sticky in stickies:
-            self.eventReceived(request, 'add', sticky)
-        store.subscribe(self.board_id, partial(self.eventReceived, request))
+        store.getStickies(self.board_id).addCallback(self.gotStickies, request)
         return NOT_DONE_YET
 
 
+    def gotStickies(self, stickies, request):
+        for sticky in stickies:
+            self.eventReceived(request, 'add', sticky)
+        store.subscribe(self.board_id, partial(self.eventReceived, request))
+
+
     def eventReceived(self, request, event, data):
+        log.msg('eventReceived %r %r %r' % (request, event, data))
         request.write(sseMsg(event, data))
 
 
@@ -167,9 +332,11 @@ if __name__ == '__main__':
     from twisted.internet import reactor
     from twisted.web.static import File
     from twisted.web.server import Site
-    from twisted.python import log
     fh = open('twistd.log', 'wb')
+    import sys
+    log.startLogging(sys.stdout)
     log.startLogging(fh)
+
     root = Resource()
     root.putChild('', Chalkboard(1))
     root.putChild('js', File('js'))
