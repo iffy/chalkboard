79fe60fc2307e9a7de15b1f7801fbe4500a3d4e9
Ubuntu <ubuntu@ip-10-170-254-121.us-west-1.compute.internal>
2013-02-07 02:06:01 +0000

    works right and logs sanely

--- server.go
+++ server.go
@@ -1,42 +1,36 @@
 package main
 
 import (
-	//"database/sql"
-	"strconv"
 	"encoding/json"
 	"fmt"
-	_ "github.com/mattn/go-sqlite3"
 	"log"
 	"net/http"
 	"path/filepath"
 	"runtime"
-	_ "time"
+	"strconv"
 )
-
 
 func f(fname string) func(w http.ResponseWriter, r *http.Request) {
 	res := func(w http.ResponseWriter, r *http.Request) {
-		log.Println(fname)
-		log.Println(*r)
 		http.ServeFile(w, r, fname)
 	}
 	return res
 }
 
 type Store interface {
-	Itersticky(func (Sticky))
+	Itersticky(func(Sticky))
 	AddSticky(Sticky) Sticky
-	RmSticky(int) 
+	RmSticky(int)
 	UpdateSticky(Sticky)
 }
 
 type MemStore struct {
 	stickies map[int]Sticky
-	Id int
+	Id       int
 }
 
-func (m *MemStore) Itersticky(fn func (Sticky)) {
-	for _, v := range(m.stickies) {
+func (m *MemStore) Itersticky(fn func(Sticky)) {
+	for _, v := range m.stickies {
 		fn(v)
 	}
 }
@@ -59,13 +53,14 @@ func (m *MemStore) UpdateSticky(a Sticky) {
 type ResponseWriter struct {
 	done   chan bool
 	writer http.ResponseWriter
+	req http.Request
 }
 
 type Sticky struct {
-	Id   int 	`json:"id"`
-	Note string	`json:"note"`
-	X    int	`json:"x"`
-	Y    int	`json:"y"`
+	Id   int    `json:"id"`
+	Note string `json:"note"`
+	X    int    `json:"x"`
+	Y    int    `json:"y"`
 }
 
 func NewEv(s Store) Ev {
@@ -74,7 +69,7 @@ func NewEv(s Store) Ev {
 
 type Ev struct {
 	subscribed chan ResponseWriter
-	events chan string
+	events     chan string
 	clients    map[string]ResponseWriter
 	Store
 }
@@ -87,9 +82,9 @@ func mkev(evt, data string) (res string) {
 	return
 }
 
-func (e *Ev) subscribe(w http.ResponseWriter) (done chan bool) {
+func (e *Ev) subscribe(w http.ResponseWriter, req http.Request) (done chan bool) {
 	done = make(chan bool)
-	r := ResponseWriter{done, w}
+	r := ResponseWriter{done, w, req}
 	e.subscribed <- r
 	return done
 }
@@ -98,17 +93,22 @@ func (e *Ev) work() {
 	for {
 		select {
 		case rw := <-e.subscribed:
-			s := fmt.Sprintf("%s", rw)
-			e.clients[s] = rw
-			go e.Itersticky(func (st Sticky) {
+			e.clients[rw.req.RemoteAddr] = rw
+			log.Printf("adding client %s", rw.req.RemoteAddr)
+			e.Itersticky(func(st Sticky) {
 				a, _ := json.Marshal(st)
-				e.events <- mkev("add", string(a))
-			})				
-			log.Printf("%s", rw.writer)
+				ev := mkev("add", string(a))
+				fmt.Fprintf(rw.writer, ev)
+				flush(rw.writer)
+			})
 		case ev := <-e.events:
-			for _, w := range e.clients {
-				log.Printf("send event %s to %s", ev, w)
-				fmt.Fprintf(w.writer, ev)
+			log.Printf("Broadcast to %d clients", len(e.clients))
+			for k, w := range e.clients {
+				_, err := fmt.Fprintf(w.writer, ev)
+				if err != nil {
+					log.Printf("removing client for %s", err)
+					delete(e.clients, k)
+				}
 				flush(w.writer)
 			}
 		}
@@ -132,7 +132,6 @@ func flush(w http.ResponseWriter) {
 
 	if fw, ok := w.(http.Flusher); ok {
 		fw.Flush()
-		log.Println("Flush")
 	}
 }
 
@@ -157,11 +156,10 @@ func main() {
 				log.Println(e)
 				return
 			}
-			log.Println(r.Form)
 			formdat := r.Form
-			switch formdat["action"][0] {
+			action := formdat["action"][0]
+			switch action {
 			case "add":
-				log.Println("add!")
 				note := formdat["note"][0]
 				x, _ := strconv.Atoi(formdat["x"][0])
 				y, _ := strconv.Atoi(formdat["y"][0])
@@ -176,18 +174,15 @@ func main() {
 				flush(w)
 				e.events <- mkev("add", string(a))
 			case "update":
-				log.Println("update!")
 				id, _ := strconv.Atoi(formdat["id"][0])
 				note := formdat["note"][0]
 				x, _ := strconv.Atoi(formdat["x"][0])
 				y, _ := strconv.Atoi(formdat["y"][0])
 				st := Sticky{id, note, x, y}
 				e.UpdateSticky(st)
-//				r := fmt.Sprintf(`"%d"`, id)
 				a, _ := json.Marshal(st)
 				e.events <- mkev("update", string(a))
 			case "remove":
-				log.Println("remove")
 				id, _ := strconv.Atoi(formdat["id"][0])
 				e.RmSticky(id)
 				r := fmt.Sprintf(`"%d"`, id)
@@ -202,9 +197,8 @@ func main() {
 		w.Header().Set("Content-type", "text/event-stream")
 		fmt.Fprintf(w, mkev("hello", `"keep alive"`))
 		flush(w)
-		done := e.subscribe(w)
+		done := e.subscribe(w, *r)
 		<-done
-		log.Println("all done %s", *r)
 	})
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
